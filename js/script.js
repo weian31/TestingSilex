5d11bbd7e6f039f254b2977b28fe385f1691fb45
weian31 <31751979+weian31@users.noreply.github.com>
2022-12-30 11:44:31 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
     window.silex = window.silex || {}
     window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+$(function(){
+    // here, the website is loaded
+    // this will fade the body out and then fade in again
+    $('button').click(function(){
+        $.get('https://simple-books-api.glitch.me/status',  // url
+      function (data, textStatus, jqXHR) {  // success callback
+          alert('status: ' + textStatus + ', data:' + data);
+    });
+    })
+});
